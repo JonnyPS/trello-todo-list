78b5721bcd31a5115a9b0d8836353fcee3479faa
Jonny Spry <jspry@energyinst.org>
2019-10-22 14:46:52 +0100

    change ResultsMessage component from a class component to a functional component

--- src/components/result-message.js
+++ src/components/result-message.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 
-class ResultMessage extends Component {
-  render() {
-    return (
-      <div {...this.props} className={this.props.classvalue}>
-        <p>{this.props.message}</p>
-      </div>
-    );
-  }  
+function ResultMessage(props) {
+	return 	<div className={props.classvalue}><p>{props.message}</p></div>
 }
 
 export default ResultMessage;
